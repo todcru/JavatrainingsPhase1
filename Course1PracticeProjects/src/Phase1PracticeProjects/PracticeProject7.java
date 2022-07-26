16126c99fc8ddaaada6648b6c91c986a0dca6327
todcru <aswin2017official@gmail.com>
2022-07-26 14:40:12 +0530

    changes

--- Course1PracticeProjects/src/Phase1PracticeProjects/PracticeProject7.java
+++ Course1PracticeProjects/src/Phase1PracticeProjects/PracticeProject7.java
@@ -17,46 +17,46 @@ public class PracticeProject7 {
 	}
 }
 
-
-public class PracticeProject72 {
-
-private String msg="Inner Classes";
-
-void display(){  
-	 class Inner{  
-		 void msg(){
-			 System.out.println(msg);
-		 }  
- }  
- 
- Inner l=new Inner();  
- l.msg();  
-}  
-
-
-public static void main(String[] args) {
-	PracticeProject72  ob=new PracticeProject72 ();  
-	ob.display();  
-	}
-}
-
-
-//anonymous inner class
-abstract class AnonymousInnerClass {
-	   public abstract void display();
-	}
-
-
-	public class PracticeProject73 {
-
-	public static void main(String[] args) {
-	AnonymousInnerClass i = new AnonymousInnerClass() {
-
-	         public void display() {
-	            System.out.println("Anonymous Inner Class");
-	         }
-	      };
-	      i.display();
-	   }
-	}
-
+//
+//public class PracticeProject72 {
+//
+//private String msg="Inner Classes";
+//
+//void display(){  
+//	 class Inner{  
+//		 void msg(){
+//			 System.out.println(msg);
+//		 }  
+// }  
+// 
+// Inner l=new Inner();  
+// l.msg();  
+//}  
+//
+//
+//public static void main(String[] args) {
+//	PracticeProject72  ob=new PracticeProject72 ();  
+//	ob.display();  
+//	}
+//}
+//
+//
+////anonymous inner class
+//abstract class AnonymousInnerClass {
+//	   public abstract void display();
+//	}
+//
+//
+//	public class PracticeProject73 {
+//
+//	public static void main(String[] args) {
+//	AnonymousInnerClass i = new AnonymousInnerClass() {
+//
+//	         public void display() {
+//	            System.out.println("Anonymous Inner Class");
+//	         }
+//	      };
+//	      i.display();
+//	   }
+//	}
+//
