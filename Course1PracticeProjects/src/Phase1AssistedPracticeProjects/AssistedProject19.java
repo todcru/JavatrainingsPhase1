f2dc500f9155e885564d3fb4f0ad99bf3e95edc9
todcru <aswin2017official@gmail.com>
2022-07-29 17:41:38 +0530

    diamond problem using OOPs’ concepts 23/07/22

---
+++ Course1PracticeProjects/src/Phase1AssistedPracticeProjects/AssistedProject19.java
@@ -0,0 +1,29 @@
+package Phase1AssistedPracticeProjects;
+
+interface First 
+{  
+    default void show() 
+    { 
+        System.out.println("Default First"); 
+    } 
+} 
+interface Second 
+{  
+    default void show() 
+    { 
+        System.out.println("Default Second"); 
+    } 
+}  
+public class AssistedProject19 implements First, Second 
+{  
+    public void show() 
+    {  
+        First.super.show(); 
+        Second.super.show(); 
+    } 
+    public static void main(String args[]) 
+    { 
+    	AssistedProject19 ob = new AssistedProject19(); 
+        ob.show(); 
+    } 
+}
