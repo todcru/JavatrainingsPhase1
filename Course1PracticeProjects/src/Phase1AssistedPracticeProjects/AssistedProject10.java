6480fd933bb56c18185194a130c1dcda6ec51bf0
todcru <aswin2017official@gmail.com>
2022-07-29 18:05:09 +0530

    implementations of regular expressions 19/07/22

--- Course1PracticeProjects/src/Phase1AssistedPracticeProjects/AssistedProject10.java
+++ Course1PracticeProjects/src/Phase1AssistedPracticeProjects/AssistedProject10.java
@@ -7,7 +7,7 @@ public class AssistedProject10 {
 public static void main(String[] args) {
 
 	String pattern = "[a-z]+";
-	String check = "Regular Expressions";
+	String check = "Regular Expression";
 	Pattern p = Pattern.compile(pattern);
 	Matcher c = p.matcher(check);
 	
