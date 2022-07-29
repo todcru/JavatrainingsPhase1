317891ba9005417aeed70f4d130238289e81c2e7
todcru <aswin2017official@gmail.com>
2022-07-29 17:37:32 +0530

    demonstrate the uses of classes & objects 23/07/22

---
+++ Course1PracticeProjects/src/Phase1AssistedPracticeProjects/AssistedProject18.java
@@ -0,0 +1,43 @@
+package Phase1AssistedPracticeProjects;
+
+
+public class AssistedProject18 
+{  
+    String name; 
+    String breed; 
+    int age; 
+    String color; 
+    public AssistedProject18(String name, String breed, int age, String color) 
+    { 
+        this.name = name; 
+        this.breed = breed; 
+        this.age = age; 
+        this.color = color; 
+    } 
+    public String getName() 
+    { 
+        return name; 
+    } 
+    public String getBreed() 
+    { 
+        return breed; 
+    } 
+    public int getAge() 
+    { 
+        return age; 
+    } 
+    public String getColor() 
+    { 
+        return color; 
+    } 
+    @Override
+    public String toString() 
+    { 
+        return("Hi my name is "+ this.getName()+ ".\nMy breed,age and color are " + this.getBreed()+", " + this.getAge()+ ", and"+ this.getColor() + "."); 
+    } 
+    public static void main(String[] args) 
+    { 
+    	AssistedProject18 scott = new AssistedProject18("Scott","papillon", 5, "black"); 
+        System.out.println(scott.toString()); 
+    } 
+}
