c42e6562895dee57bd4b3380155ef92aa84bdf98
todcru <aswin2017official@gmail.com>
2022-07-29 16:31:43 +0530

    demonstrate try and catch 23/07/22

---
+++ Course1PracticeProjects/src/Phase1AssistedPracticeProjects/AssistedProject14.java
@@ -0,0 +1,21 @@
+package Phase1AssistedPracticeProjects;
+
+public class AssistedProject14 
+{
+    public static void main(String args[]) 
+    {
+        int[] array = new int[3];
+        try 
+        {
+            array[7] = 3;
+        }
+        catch (ArrayIndexOutOfBoundsException e) 
+        {
+            System.out.println("Array index is out of bounds!"); 
+        }
+        finally 
+        {
+            System.out.println("The array is of size " + array.length);
+        }
+    }
+}
